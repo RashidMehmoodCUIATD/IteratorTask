5e5ba5b21af160297a8de3b27318ab8233906d41
RashidMehmood <syedrashid47@gmail.com>
2022-10-19 06:42:55 +0500

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    int ID=0;
+    String Name="";
+    int Age=0;
+    String Department="";
+    double Salary=0.00;
+
+    public Employee(int id, String nam, int age, String dep, double salary)
+    {
+        this.ID= id;
+        this.Name=nam;
+        this.Age=age;
+        this.Department=dep;
+        this.Salary=salary;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Organization Or1=new Organization();
+
+        Or1.Employees.add(new Employee(1,"Adil", 34,"Taxation",78000.56));
+        Or1.Employees.add(new Employee(2,"Rashid", 35,"Management",38000.56));
+        Or1.Employees.add(new Employee(3,"kashif", 36,"ObBoarding",48000.56));
+        Or1.Employees.add(new Employee(4,"altaf", 37,"Marketing",28000.56));
+        Or1.Employees.add(new Employee(5,"haider", 38,"Taxation",98000.56));
+        Or1.Employees.add(new Employee(6,"majid", 39,"Management",68000.56));
+        Or1.Employees.add(new Employee(7,"sarfaraz", 40,"Taxation",58000.56));
+        Or1.Employees.add(new Employee(8,"babar", 23,"ObBoarding",18000.56));
+        Or1.Employees.add(new Employee(9,"najam", 19,"Marketing",88000.56));
+
+       
+
+
+
+
+    }
+}
